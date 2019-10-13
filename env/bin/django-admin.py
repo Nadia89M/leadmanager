b064e2c4976f3a50c2e75565c92512c03a08bb84
Nadia <nadia.mohamed89@gmail.com>
2019-10-13 16:36:56 +0200

    first commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/nadia/Desktop/Giacomo/lead_manager_react_django/leadmanager/env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
