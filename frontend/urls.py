3a6721c0b9fe73ed3425b0f37b1cf572ae101e7b
Nadia <nadia.mohamed89@gmail.com>
2019-12-01 02:33:53 +0100

    Remove Corsheader

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('*', views.handler404),
+    path('*', views.handler500)
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
+
 def index(request):
-  return render(request, 'frontend/index.html')
+    return render(request, 'frontend/index.html')
+
+
+def handler404(request):
+    return render(request, '404.html', status=404)
+
+
+def handler500(request):
+    return render(request, '500.html', status=500)
