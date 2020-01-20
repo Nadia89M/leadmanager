88c746239993f6bf2f9957313c0a5bcac9b7429f
Nadia <nadia.mohamed89@gmail.com>
2020-01-20 19:26:09 +0100

    Update tables and notifications

--- frontend/src/components/layout/Header2.js
+++ frontend/src/components/layout/Header2.js
@@ -1,64 +1,69 @@
 import React, { Component, Fragment } from 'react';
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
-import { filteredLeads, getLeads } from "../../actions/leads";
-import Notification from "./Notification";
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { filteredLeads, getLeads } from '../../actions/leads';
+import Notification from './Notification';
 
 class Header2 extends Component {
-    static propTypes = {
-        auth: PropTypes.object.isRequired,
-        filteredLeads: PropTypes.func.isRequired,
-        filteredLeadsGroup: PropTypes.array.isRequired,
-        proposalGroup: PropTypes.array.isRequired,
-        getLeads: PropTypes.func.isRequired
-    };
+  static propTypes = {
+    auth: PropTypes.object.isRequired,
+    filteredLeads: PropTypes.func.isRequired,
+    filteredLeadsGroup: PropTypes.array.isRequired,
+    proposalGroup: PropTypes.array.isRequired,
+    getLeads: PropTypes.func.isRequired
+  };
 
-    componentDidMount() {
-        this.props.getLeads();
-        setTimeout(
-            function () {
-                this.props.filteredLeads();
-            }
-                .bind(this),
-            500
-        );
-    }
+  componentDidMount() {
+    this.props.getLeads();
+    setTimeout(
+      function() {
+        this.props.filteredLeads();
+      }.bind(this),
+      500
+    );
+  }
 
-    render() {
-        const { isAuthenticated } = this.props.auth;
+  render() {
+    const { isAuthenticated } = this.props.auth;
 
-        return (
-            <Fragment>
-                <div className="header-area">
-                    <div className="row align-items-center">
-                        <div className="col-md-6 col-sm-8 clearfix">
-                            <div className="nav-btn pull-left">
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                            </div>
-                        </div>
-                        <div className="col-md-6 col-sm-4 clearfix">
-                            <ul className="notification-area pull-right">
-                                <li id="full-view"><i className="ti-fullscreen"></i></li>
-                                <li id="full-view-exit"><i className="ti-zoom-out"></i></li>
-                                {(isAuthenticated) ? <Notification /> : ""}
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </Fragment>
-        )
-    }
+    return (
+      <Fragment>
+        <div className='header-area'>
+          <div className='row align-items-center'>
+            <div className='col-md-6 col-sm-8 clearfix'>
+              <div className='nav-btn pull-left'>
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+            </div>
+            <div className='col-md-6 col-sm-4 clearfix'>
+              <ul className='notification-area pull-right'>
+                <li id='full-view'>
+                  <i className='ti-fullscreen'></i>
+                </li>
+                <li id='full-view-exit'>
+                  <i className='ti-zoom-out'></i>
+                </li>
+                {isAuthenticated && window.location.href.endsWith('/') ? (
+                  <Notification />
+                ) : (
+                  ''
+                )}{' '}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-    auth: state.auth,
-    leads: state.leads.leads,
-    filteredLeadsGroup: state.leads.filteredLeadsGroup,
-    proposalGroup: state.leads.proposalGroup,
+  auth: state.auth,
+  leads: state.leads.leads,
+  filteredLeadsGroup: state.leads.filteredLeadsGroup,
+  proposalGroup: state.leads.proposalGroup
 });
 
-export default connect(
-    mapStateToProps, { filteredLeads, getLeads }
-)(Header2);
+export default connect(mapStateToProps, { filteredLeads, getLeads })(Header2);
--- frontend/src/components/leads/Leads.js
+++ frontend/src/components/leads/Leads.js
@@ -1,9 +1,9 @@
-import React, { Component, Fragment } from "react";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
-import { getLeads, deleteLead, setCurrent } from "../../actions/leads";
-import EditLead from "./EditLead";
-import ShowLead from "./ShowLead";
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getLeads, deleteLead, setCurrent } from '../../actions/leads';
+import EditLead from './EditLead';
+import ShowLead from './ShowLead';
 
 import { withTranslation } from 'react-i18next';
 
@@ -21,157 +21,191 @@ export class LeadsT extends Component {
   }
 
   deleteLead = id => {
-    this.props.deleteLead(id)
+    this.props.deleteLead(id);
     this.props.history.push('/#/dashboard');
-  }
+  };
 
   render() {
     const { t } = this.props;
 
     return (
       <Fragment>
-        <div className="modal fade" id="editModal" tabIndex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
-          <div className="modal-dialog" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel">{t('title1.label')}</h5>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
+        <div
+          className='modal fade'
+          id='editModal'
+          tabIndex='-1'
+          role='dialog'
+          aria-labelledby='editModalLabel'
+          aria-hidden='true'
+        >
+          <div className='modal-dialog' role='document'>
+            <div className='modal-content'>
+              <div className='modal-header'>
+                <h5 className='modal-title' id='exampleModalLabel'>
+                  {t('title1.label')}
+                </h5>
+                <button
+                  type='button'
+                  className='close'
+                  data-dismiss='modal'
+                  aria-label='Close'
+                >
+                  <span aria-hidden='true'>&times;</span>
                 </button>
               </div>
-              <div className="modal-body">
+              <div className='modal-body'>
                 <EditLead current={this.props.currentLead} />
               </div>
             </div>
           </div>
         </div>
-        <div className="modal fade" id="showModal" tabIndex="-1" role="dialog" aria-labelledby="showModalLabel" aria-hidden="true">
-          <div className="modal-dialog" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel">{t('title2.label')}</h5>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
+        <div
+          className='modal fade'
+          id='showModal'
+          tabIndex='-1'
+          role='dialog'
+          aria-labelledby='showModalLabel'
+          aria-hidden='true'
+        >
+          <div className='modal-dialog' role='document'>
+            <div className='modal-content'>
+              <div className='modal-header'>
+                <h5 className='modal-title' id='exampleModalLabel'>
+                  {t('title2.label')}
+                </h5>
+                <button
+                  type='button'
+                  className='close'
+                  data-dismiss='modal'
+                  aria-label='Close'
+                >
+                  <span aria-hidden='true'>&times;</span>
                 </button>
               </div>
-              <div className="modal-body">
+              <div className='modal-body'>
                 <ShowLead />
               </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-secondary" data-dismiss="modal">{t('action2.label')}</button>
+              <div className='modal-footer'>
+                <button
+                  type='button'
+                  className='btn btn-secondary'
+                  data-dismiss='modal'
+                >
+                  {t('action2.label')}
+                </button>
               </div>
             </div>
           </div>
         </div>
-        <h4 className="pb-3 page-title pull-left">Leads</h4>
-        <table className="table table-striped">
+        <h4 className='pb-3 page-title pull-left'>Leads</h4>
+        <table className='table table-striped'>
           <thead>
             <tr>
               <th>{t('data1.label')}</th>
-              <th className="d-none d-lg-table-cell">{t('data2.label')}</th>
+              <th className='d-none d-lg-table-cell'>{t('data2.label')}</th>
               <th>{t('data3.label')}</th>
-              <th className="d-none d-xl-table-cell">{t('data4.label')}</th>
+              <th className='d-none d-xl-table-cell'>{t('data4.label')}</th>
               <th>{t('data5.label')}</th>
-              <th className="d-none d-xl-table-cell">{t('data6.label')}</th>
-              <th className="d-none d-xl-table-cell">{t('data7.label')}</th>
-              <th className="d-none d-lg-table-cell">{t('data8.label')}</th>
-              <th className="d-none d-xl-table-cell">{t('data9.label')}</th>
-              <th className="d-none d-xl-table-cell">{t('data10.label')}</th>
-              <th className="d-none d-xl-table-cell">{t('data11.label')}</th>
+              <th className='d-none d-xl-table-cell'>{t('data6.label')}</th>
+              <th className='d-none d-xl-table-cell'>{t('data7.label')}</th>
+              <th className='d-none d-lg-table-cell'>{t('data8.label')}</th>
+              <th className='d-none d-xl-table-cell'>{t('data9.label')}</th>
+              <th className='d-none d-xl-table-cell'>{t('data10.label')}</th>
+              <th className='d-none d-xl-table-cell'>{t('data11.label')}</th>
               <th />
             </tr>
           </thead>
           <tbody>
             {this.props.searchedLeads.map(lead => (
-              <tr key={lead.id} >
+              <tr key={lead.id}>
                 <td>{lead.name}</td>
-                <td className="d-none d-lg-table-cell">{lead.email}</td>
-                <td>{lead.company}</td>
-                <td className="d-none d-xl-table-cell">{lead.city}</td>
+                <td className='d-none d-lg-table-cell'>{lead.email}</td>
+                <td>{lead.number}</td>
+                <td className='d-none d-xl-table-cell'>{lead.company}</td>
                 <td>{lead.region}</td>
-                <td className="d-none d-xl-table-cell">{lead.district}</td>
-                <td className="d-none d-xl-table-cell">{lead.address}</td>
-                <td className="d-none d-lg-table-cell">{lead.number}</td>
-                <td className="d-none d-xl-table-cell">{lead.last_action}</td>
-                <td className="d-none d-xl-table-cell">
-                  {
-                    lead.to_be_contacted_on == "SOON" ? `${t('month.label')}` : ""
-                  }
-                  {
-                    lead.to_be_contacted_on == "JANUARY" ? `${t('month1.label')}` : ""
-                  }
-                  {
-                    lead.to_be_contacted_on == "FEBRUARY" ? `${t('month2.label')}` : ""
-                  }
-                  {
-                    lead.to_be_contacted_on == "MARCH" ? `${t('month3.label')}` : ""
-                  }
-                  {
-                    lead.to_be_contacted_on == "APRIL" ? `${t('month4.label')}` : ""
-                  }
-                  {
-                    lead.to_be_contacted_on == "MAY" ? `${t('month5.label')}` : ""
-                  }
-                  {
-                    lead.to_be_contacted_on == "JUNE" ? `${t('month6.label')}` : ""
-                  }
-                  {
-                    lead.to_be_contacted_on == "JULY" ? `${t('month7.label')}` : ""
-                  }
-                  {
-                    lead.to_be_contacted_on == "AUGUST" ? `${t('month8.label')}` : ""
-                  }
-                  {
-                    lead.to_be_contacted_on == "SEPTEMBER" ? `${t('month9.label')}` : ""
-                  }
-                  {
-                    lead.to_be_contacted_on == "OCTOBER" ? `${t('month10.label')}` : ""
-                  }
-                  {
-                    lead.to_be_contacted_on == "NOVEMBER" ? `${t('month11.label')}` : ""
-                  }
-                  {
-                    lead.to_be_contacted_on == "DECEMBER" ? `${t('month12.label')}` : ""
-                  }
+                <td className='d-none d-xl-table-cell'>{lead.district}</td>
+                <td className='d-none d-xl-table-cell'>{lead.city}</td>
+                <td className='d-none d-lg-table-cell'>{lead.address}</td>
+                <td className='d-none d-xl-table-cell'>{lead.last_action}</td>
+                <td className='d-none d-xl-table-cell'>
+                  {lead.to_be_contacted_on == 'SOON'
+                    ? `${t('month.label')}`
+                    : ''}
+                  {lead.to_be_contacted_on == 'JANUARY'
+                    ? `${t('month1.label')}`
+                    : ''}
+                  {lead.to_be_contacted_on == 'FEBRUARY'
+                    ? `${t('month2.label')}`
+                    : ''}
+                  {lead.to_be_contacted_on == 'MARCH'
+                    ? `${t('month3.label')}`
+                    : ''}
+                  {lead.to_be_contacted_on == 'APRIL'
+                    ? `${t('month4.label')}`
+                    : ''}
+                  {lead.to_be_contacted_on == 'MAY'
+                    ? `${t('month5.label')}`
+                    : ''}
+                  {lead.to_be_contacted_on == 'JUNE'
+                    ? `${t('month6.label')}`
+                    : ''}
+                  {lead.to_be_contacted_on == 'JULY'
+                    ? `${t('month7.label')}`
+                    : ''}
+                  {lead.to_be_contacted_on == 'AUGUST'
+                    ? `${t('month8.label')}`
+                    : ''}
+                  {lead.to_be_contacted_on == 'SEPTEMBER'
+                    ? `${t('month9.label')}`
+                    : ''}
+                  {lead.to_be_contacted_on == 'OCTOBER'
+                    ? `${t('month10.label')}`
+                    : ''}
+                  {lead.to_be_contacted_on == 'NOVEMBER'
+                    ? `${t('month11.label')}`
+                    : ''}
+                  {lead.to_be_contacted_on == 'DECEMBER'
+                    ? `${t('month12.label')}`
+                    : ''}
                 </td>
-                <td className="d-none d-xl-table-cell">
-                  {
-                    lead.status == "NEW" ? `${t('status1.label')}` : ""
-                  }
-                  {
-                    lead.status == "CONTACTED" ? `${t('status2.label')}` : ""
-                  }
-                  {
-                    lead.status == "CONNECTED" ? `${t('status3.label')}` : ""
-                  }
-                  {
-                    lead.status == "OPEN" ? `${t('status4.label')}` : ""
-                  }
-                  {
-                    lead.status == "LEAD" ? `${t('status5.label')}` : ""
-                  }
-                  {
-                    lead.status == "CLIENT" ? `${t('status6.label')}` : ""
-                  }
-                  {
-                    lead.status == "UNQUALIFIED" ? `${t('status7.label')}` : ""
-                  }
+                <td className='d-none d-xl-table-cell'>
+                  {lead.status == 'NEW' ? `${t('status1.label')}` : ''}
+                  {lead.status == 'CONTACTED' ? `${t('status2.label')}` : ''}
+                  {lead.status == 'CONNECTED' ? `${t('status3.label')}` : ''}
+                  {lead.status == 'OPEN' ? `${t('status4.label')}` : ''}
+                  {lead.status == 'LEAD' ? `${t('status5.label')}` : ''}
+                  {lead.status == 'CLIENT' ? `${t('status6.label')}` : ''}
+                  {lead.status == 'UNQUALIFIED' ? `${t('status7.label')}` : ''}
                 </td>
                 <td>
-                  <div className="d-flex justify-content-center">
-                    <a className="px-2" href="#" data-toggle="modal" data-target="#showModal" onClick={this.props.setCurrent.bind(this, lead.id)}>
-                      {" "}
-                      <i className="far fa-eye"></i>
+                  <div className='d-flex justify-content-center'>
+                    <a
+                      className='px-2'
+                      href='#'
+                      data-toggle='modal'
+                      data-target='#showModal'
+                      onClick={this.props.setCurrent.bind(this, lead.id)}
+                    >
+                      {' '}
+                      <i className='far fa-eye'></i>
                     </a>
-                    <a className="px-2" href="#" data-toggle="modal" data-target="#editModal" onClick={this.props.setCurrent.bind(this, lead.id)}>
-                      {" "}
-                      <i className="fa fa-edit"></i>
+                    <a
+                      className='px-2'
+                      href='#'
+                      data-toggle='modal'
+                      data-target='#editModal'
+                      onClick={this.props.setCurrent.bind(this, lead.id)}
+                    >
+                      {' '}
+                      <i className='fa fa-edit'></i>
                     </a>
-                    <a className="px-2" href="#"
+                    <a
+                      className='px-2'
+                      href='#'
                       onClick={this.deleteLead.bind(this, lead.id)}
                     >
-                      {" "}
-                      <i className="ti-trash"></i>
+                      {' '}
+                      <i className='ti-trash'></i>
                     </a>
                   </div>
                 </td>
@@ -184,14 +218,13 @@ export class LeadsT extends Component {
   }
 }
 
-const Leads = withTranslation()(LeadsT)
+const Leads = withTranslation()(LeadsT);
 
 const mapStateToProps = state => ({
   leads: state.leads.leads,
-  searchedLeads: state.leads.searchedLeads,
+  searchedLeads: state.leads.searchedLeads
 });
 
-export default connect(
-  mapStateToProps,
-  { getLeads, deleteLead, setCurrent }
-)(Leads);
+export default connect(mapStateToProps, { getLeads, deleteLead, setCurrent })(
+  Leads
+);
